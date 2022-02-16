def5cf4d2bc690f98a69f176f3b47f1d97f20c69
Jorge Ramos <jorgeramos2068@gmail.com>
2022-02-16 16:21:12 -0600

    Create increase and decrease methods in counter component

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,6 +1,10 @@
 <template>
   <h2>Counter</h2>
-  <p>{{ counter }} <sup>2</sup> = {{ counter * counter }}</p>
+  <p>{{ counter }} <sup>2</sup> = {{ squaredCounter }}</p>
+  <div>
+    <button v-on:click="increase()">+1</button>
+    <button v-on:click="decrease()">-1</button>
+  </div>
 </template>
 
 <script>
@@ -11,7 +15,37 @@ export default {
       counter: 5,
     };
   },
+  methods: {
+    increase() {
+      this.counter++;
+    },
+    decrease() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squaredCounter() {
+      console.log('Computed counter');
+      return this.counter * this.counter;
+    },
+  },
 };
 </script>
 
-<style></style>
+<style>
+button {
+  background-color: #64bb87;
+  border: 1px solid white;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin: 0 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
